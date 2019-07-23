b1a0e4cf577cdcdc52cd74387e932a82420b1ffd
Isis Lázaro <isislazaro@gmail.com>
2019-07-23 11:27:35 -0500

    mapa funcionando

--- mapping.js
+++ mapping.js
@@ -1,12 +1,12 @@
 mapContainer = document.getElementById("map-container");
 
 // let mymap = L.map(mapContainer).setView([51.505, -0.09], 13);
-/* L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiaW1sciIsImEiOiJjanljbWowbDgwanZlM2xzOGZqcDFxbHVuIn0.niCENs8TKhS-7PSMjHFE3A'
-}).addTo(mymap); */
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox.streets',
+//     accessToken: 'pk.eyJ1IjoiaW1sciIsImEiOiJjanljbWowbDgwanZlM2xzOGZqcDFxbHVuIn0.niCENs8TKhS-7PSMjHFE3A'
+// }).addTo(mymap);
 
 let map = L.map(mapContainer).fitWorld();
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiaW1sciIsImEiOiJjanljbWowbDgwanZlM2xzOGZqcDFxbHVuIn0.niCENs8TKhS-7PSMjHFE3A', {
@@ -16,7 +16,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
 		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 		id: 'mapbox.streets'
 }).addTo(map);
-// map.locate({setView: true, maxZoom: 16});
+map.locate({setView: true, maxZoom: 16});
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
 
@@ -26,4 +26,4 @@ function onLocationFound(e) {
     //L.circle(e.latlng, radius).addTo(map);
 }
 
-// map.on('locationfound', onLocationFound);
+map.on('locationfound', onLocationFound);
